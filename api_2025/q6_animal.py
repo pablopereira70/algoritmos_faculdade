5d7a1aa451635ba1bb39c4b00c3c04ca305d4c8f
Pablo Pereira <pabloleonpereira70@gmail.com>
2026-04-07 19:44:55 -0300

    dando andamento a api_2025

--- api_2025/q6_animal.py
+++ api_2025/q6_animal.py
@@ -13,9 +13,31 @@ def main():
         comando = obter_inteiro_intervalo("--> ", 0, 1)
 
         if comando == 1:
-            ...
+            quantidade = 0
+            somatorio = 0
+            animal = obter_texto_minimo_e_maximo("Digite o nome de um animal: ", 7)
+            for i in animal:
+                inteiro = obter_inteiro("Digite um número inteiro: ")
+                eh_primo = verificar_eh_primo(inteiro)
+                if eh_primo:
+                    break
+                somatorio += inteiro
+                quantidade += 1
+            if quantidade == 0:
+                quantidade = 1
+            print(f"somatório = {somatorio}")
+            print(f"média = {somatorio/quantidade}")
         else:
             break
+
+
+def verificar_eh_primo(n): 
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 main()
